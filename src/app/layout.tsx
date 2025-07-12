f495b31d4aab8cc4e07ffc4d9e0f47def0789640
seoyoon park <dev.yelee@gmail.com>
2025-07-12 17:36:56 +0900

    fix: tailwind 적용되지 않는 버그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '메가존 구내식당',
   description: '메가존 구내식당 식단표',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang='ko'>
       <body className={inter.className}>
-        <main className="min-h-screen bg-gradient-to-br from-[#EFF6FF] via-[#EFF6FF] to-[#FAF5FF]">
+        <main className='min-h-screen bg-gradient-to-br from-[#EFF6FF] via-[#EFF6FF] to-[#FAF5FF]'>
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
