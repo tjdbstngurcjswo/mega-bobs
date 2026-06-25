9a5d618a346f46fff55f518caf89d1aa29643a14
yelee park <dev.yelee@gmail.com>
2026-06-25 23:28:43 +0900

    feat: [이스터에그] 개발자 콘솔 ASCII 아트 출력

---
+++ src/components/@shared/ConsoleArt/ConsoleArt.constants.ts
@@ -0,0 +1,16 @@
+export const CONSOLE_ART = ` ___________________
+/                   \\
+|    MegaBobs!      |
+\\___________________/
+          \\
+           \\
+
+    / \\__
+   (    @\\___
+   /         O
+  /   (_____/
+ /_____/
+`;
+
+export const CONSOLE_ART_STYLE =
+  'color: #e2c04c; font-family: monospace; font-size: 12px; line-height: 1.5;';
